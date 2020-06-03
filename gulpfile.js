3fbbd355cfd3b69c5450b7ce2b4b83ef673d42a2
Igor Kucherenko <igor.kucherenko.dev@gmail.com>
2020-06-04 00:04:36 +0300

    add minification to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,24 @@ const gulp = require('gulp');
 const sass = require('gulp-dart-sass');
 const concatCss = require('gulp-concat-css');
 const sync = require('browser-sync').create();
-
-const stylesPathSrc = './src/styles/**/*.scss';
-const stylesPathDest = './dist';
+const cleanCSS = require('gulp-clean-css');
 
 function generateStyles() {
-  return gulp.src(stylesPathSrc)
+  return gulp.src('./src/styles/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(concatCss('bundle.css'))
-    .pipe(gulp.dest(stylesPathDest));
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist'));
 }
 
 function generateHtml() {
   return gulp.src('./src/index.html')
     .pipe(gulp.dest('./dist/'));
+}
+
+function build() {
+  generateHtml();
+  generateStyles();
 }
 
 function browserSync() {
@@ -27,12 +31,10 @@ function browserSync() {
     }
   });
 
-
-  generateHtml();
-  generateStyles();
+  build();
 
   gulp.watch('./src/index.html', generateHtml);
-  gulp.watch(stylesPathSrc, generateStyles);
+  gulp.watch('./src/styles/**/*.scss', generateStyles);
   gulp.watch('./dist').on('change', sync.reload);
 }
 
